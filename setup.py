2303f8d5fda0f5110993fb4d982dc2e4564dffe0
vamshimohank <vamshimohank@gmail.com>
2018-09-18 10:36:51 +0200

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+
+setup(name='Orca_scripts',
+        version='0.1',
+        description='Contains routines for pasing data from a ORCA output files',
+        packages=['Orca_parse'],
+        zip_safe=False
+        )
